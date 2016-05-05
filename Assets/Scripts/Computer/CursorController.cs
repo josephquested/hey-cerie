39dab0cbd745ff9f58814622f16c81e14b6d1f6b
josephquested <josephquested@gmail.com>
2016-05-05 19:58:05 +1200

    add walls 95 window sprite

--- Assets/Scripts/Computer/CursorController.cs
+++ Assets/Scripts/Computer/CursorController.cs
@@ -4,36 +4,15 @@ using System.Collections;
 public class CursorController : MonoBehaviour {
 	[SerializeField] ComputerController computerController;
 	[SerializeField] float speed = 1.0f;
-	[SerializeField] Vector3 lastPosition;
 	[SerializeField] bool canMove;
 
   void Update () {
-		RememberLastPosition();
 		ControlMouseMovement();
   }
 
 	void ControlMouseMovement () {
 		if (computerController.inUse && canMove) {
 			transform.Translate(new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) * Time.deltaTime * speed);
-		}
-	}
-
-	void RememberLastPosition () {
-		lastPosition = transform.localPosition;
-	}
-
-	void OnTriggerStay (Collider collider) {
-		if (collider.CompareTag("Edge")) {
-			print("enter edge!");
-			canMove = false;
-			transform.localPosition = lastPosition;
-		}
-	}
-
-	void OnTriggerExit (Collider collider) {
-		if (collider.CompareTag("Edge")) {
-			print("edge exit!");
-			canMove = true;
 		}
 	}
 }
